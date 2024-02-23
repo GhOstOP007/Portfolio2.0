f175a838973fc977b26276830b722b9180673d1d
Ishan Vishwakarma <85371199+GhOstOP007@users.noreply.github.com>
2024-02-23 20:12:21 +0530

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -18,9 +18,9 @@ const Skills = (props: Props) => {
       </h3>
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 test-sm">
-        Hover Over a Skill for Current Profieciency
+        Hover Over a Skill for Current Proficiency
       </h3>
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-5 gap-5">
         {/*Upperhalf */}
         <Skill
           img="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
